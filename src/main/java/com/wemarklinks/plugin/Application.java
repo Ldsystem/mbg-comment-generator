ac612dc9b82d93abb5fee3821c2fe4dc01a4e7ca
shenglong <lsz888888@yeah.net>
2019-09-06 21:44:32 +0800

    initial commit

---
+++ src/main/java/com/wemarklinks/plugin/Application.java
@@ -0,0 +1,9 @@
+package com.wemarklinks.plugin;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/wemarklinks/plugin/CustomCommentGenerator.java
@@ -0,0 +1,59 @@
+package com.wemarklinks.plugin;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.CompilationUnit;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+
+public class CustomCommentGenerator extends DefaultCommentGenerator {
+    private boolean addRemarkComments = false;
+    private static final String EXAMPLE_SUFFIX="Example";
+    private static final String API_MODEL_PROPERTY_FULL_CLASS_NAME="io.swagger.annotations.ApiModelProperty";
+
+    /**
+     * 设置用户配置的参数
+     */
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    /**
+     * 给字段添加注释
+     */
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        //根据参数和备注信息判断是否添加备注信息
+        if(addRemarkComments&&StringUtility.stringHasValue(remarks)){
+//            addFieldJavaDoc(field, remarks);
+            //数据库中特殊字符需要转义
+            if(remarks.contains("\"")){
+                remarks = remarks.replace("\"","'");
+            }
+            String annotation = "@ApiModelProperty(value = \""+remarks + "\"";
+            if (!introspectedColumn.isNullable()) {
+                annotation = annotation + ", required = true";
+            }
+            annotation = annotation + ")";
+            //给model的字段添加swagger注解
+            field.addJavaDocLine(annotation);
+        }
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        super.addJavaFileComment(compilationUnit);
+        //只在model中添加swagger注解类的导入
+        if(!compilationUnit.isJavaInterface()&&!compilationUnit.getType().getFullyQualifiedName().contains(EXAMPLE_SUFFIX)){
+            compilationUnit.addImportedType(new FullyQualifiedJavaType(API_MODEL_PROPERTY_FULL_CLASS_NAME));
+        }
+    }
+}
